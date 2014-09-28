8abae0b1297c4736ec23f81709d9436ac9593b8a
Martin Amm <martin.amm@adenin.com>
2014-09-28 10:45:57 -0400

    - converted to vs2013 - removed old backups

---
+++ MarkdownDeepSample2013/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarkdownDeepSample2013.Startup))]
+namespace MarkdownDeepSample2013
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
